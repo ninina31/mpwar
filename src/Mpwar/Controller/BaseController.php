8a0dec39f4522693e30e402269264dba14787367
Andreina Loriente <ninina31@gmail.com>
2015-03-15 17:59:45 +0100

    Twig test

--- src/Mpwar/Controller/BaseController.php
+++ src/Mpwar/Controller/BaseController.php
@@ -1,13 +1,22 @@
 <?php
 
   namespace Mpwar\Controller;
+  use Twig\Autoloader;
 
   abstract class BaseController
   {
+    private $twig;
     
     public function __construct(argument)
     {
+      $loader = new Twig_Loader_Filesystem('../src/Views');
+      $this->twig = new Twig_Environment($loader);
       echo "Soy BaseController";
+    }
+
+    public function render($template, $data)
+    {
+      return $this->twig->render($template, $data);
     }
 
   }
