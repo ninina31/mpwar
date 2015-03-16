2e804c83743f13764a99845ae2f7823e8c79da61
Andreina Loriente <ninina31@gmail.com>
2015-03-16 21:25:56 +0100

    Modify request

--- src/Mpwar/Component/Bootstrap.php
+++ src/Mpwar/Component/Bootstrap.php
@@ -1,9 +1,18 @@
 <?php
 
   namespace Mpwar\Component\Bootstrap;
+  use Mpwar\Component\Request\Request;
+  use Mpwar\Component\Session\Session;
 
   class Bootstrap
   {
+
+    protected $request;
+
+    public function __construct()
+    {
+      $this->request = new Request(new Session);
+    }
 
     public function execute()
     {
